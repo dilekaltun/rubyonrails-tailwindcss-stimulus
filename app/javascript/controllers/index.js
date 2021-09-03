aade6da41b1d9fe6f9546556666bc5adc52d8179
Dilek Altun <altyndilek@gmail.com>
2021-09-03 14:09:21 +0300

    Ruby on Rails & Tailwind & Stimulus

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,7 @@
+// app/javascript/controllers/index.js
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context(".", true, /\.js$/)
+application.load(definitionsFromContext(context))
